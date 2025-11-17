60700bc9256436a7fdcb92fedfb44e3a8a39b0a9
S0ra <s0ra@duck.com>
2025-11-16 23:09:59 -0500

    Start working on the asciic rewrite

--- asciic/src/main.rs
+++ asciic/src/main.rs
@@ -1,349 +1,44 @@
-#![warn(clippy::pedantic)]
-
 use std::{
     error::Error,
-    fmt::Write as FmtWrite,
-    fs::{File, read_dir},
-    io::{Read, Write},
-    path::{Path, PathBuf},
-    str::FromStr,
-    sync::{
-        Arc,
-        atomic::{AtomicBool, AtomicUsize, Ordering},
-    },
+    fs::remove_dir_all,
+    sync::{Arc, atomic::Ordering::Relaxed},
 };
 
-use image::{GenericImageView, ImageError, imageops::FilterType, io::Reader};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use ron::ser::PrettyConfig;
-use tar::Builder;
-use tempfile::TempDir;
-use tokio::runtime::Runtime;
-use zstd::encode_all;
+use clap::Parser;
 
-use cli::cli;
-use primitives::{
-    Options, OutputSize,
-    PaintStyle::{self, BgOnly, BgPaint, FgPaint},
-};
-use util::{add_file, clean, clean_abort, ffmpeg, max_sub, pause};
+use crate::{cli::Args, colours::YELLOW, primitives::AsciiCompiler};
 
-use crate::{
-    installer::{setup_ffmpeg, setup_ytdlp},
-    primitives::Metadata,
-    util::{probe_fps, yt_dlp},
-};
+type Res<T> = Result<T, Box<dyn Error>>;
 
 mod cli;
-mod installer;
+mod colours;
 mod primitives;
-mod util;
-
-#[allow(clippy::too_many_lines)]
-fn main() -> Result<(), Box<dyn Error>> {
-    let matches = cli().get_matches();
-
-    let mut options = Options {
-        boost: matches.contains_id("brightness-boost"),
-        redimension: *matches.get_one::<OutputSize>("frame-size").unwrap(),
-        colorize: matches.contains_id("colorize"),
-        skip_compression: matches.contains_id("no-compression"),
-        style: *matches.get_one::<PaintStyle>("style").unwrap(),
-        compression_threshold: *matches.get_one::<u8>("compression-threshold").unwrap(),
-        skip_audio: matches.contains_id("no-audio"),
-        should_delete_video: matches.is_present("delete"),
-    };
-
-    if options.redimension == OutputSize(0, 0) {
-        let Some((width, height)) = term_size::dimensions() else {
-            return Err("Could not detect terminal window size.".into());
-        };
-
-        options.redimension = OutputSize(width, height);
-    }
-
-    let ffmpeg_flags = matches
-        .get_many::<String>("ffmpeg-flags")
-        .unwrap_or_default()
-        .collect::<Vec<_>>();
-
-    if let Some(image) = matches.get_one::<String>("image") {
-        let image_path = PathBuf::from_str(image)?;
-        let processed_img = process_image(&image_path, options)?;
-
-        File::create(format!(
-            "{}.txt",
-            image_path.file_stem().unwrap().to_str().unwrap()
-        ))?
-        .write_all(processed_img.as_bytes())?;
-        return Ok(());
-    }
-
-    let mut output = matches.get_one::<PathBuf>("output").unwrap().clone();
-
-    let rt = Runtime::new()?;
-
-    let (ffmpeg_path, ffprobe_path) = setup_ffmpeg(&rt)?;
-
-    let video_path = &{
-        if let Some(path) = matches.get_one::<String>("video") {
-            path.clone()
-        } else {
-            let ytdlp_path = setup_ytdlp(&rt)?;
-            let mp4_output = format!("{}{}", output.to_str().unwrap(), ".mp4");
-            yt_dlp(
-                &ytdlp_path,
-                matches.get_one::<String>("youtube").unwrap(),
-                &mp4_output,
-            )?;
-            mp4_output
-        }
-    };
-
-    let cloned_video_path = video_path.clone();
-
-    let fps_frametime = probe_fps(video_path, &ffprobe_path)?;
-
-    let tmp = Arc::new(TempDir::new_in(".")?);
-    let tmp_path = tmp.path();
-
-    let tmp_handler = Arc::clone(&tmp);
-
-    let should_stop = Arc::new(AtomicBool::default());
-    let stop_handle = Arc::clone(&should_stop);
-    ctrlc::set_handler(move || {
-        stop_handle.store(true, Ordering::Relaxed);
-        clean_abort(
-            tmp_handler.path(),
-            options.should_delete_video,
-            &cloned_video_path,
-        );
-    })?;
-
-    println!(">=== Running FFMPEG ===<");
-
-    // Split file into frames
-    ffmpeg(
-        &ffmpeg_path,
-        &[
-            "-r",
-            "1",
-            "-i",
-            video_path,
-            "-r",
-            "1",
-            &format!("{}/%03d.png", tmp_path.to_str().unwrap()),
-        ],
-        &ffmpeg_flags,
-    )
-    .unwrap_or_else(|_| {
-        clean_abort(tmp_path, options.should_delete_video, video_path);
-    });
-
-    // Extract audio
-    if !options.skip_audio {
-        ffmpeg(
-            &ffmpeg_path,
-            &[
-                "-i",
-                video_path,
-                &format!("{}/audio.mp3", tmp_path.to_str().unwrap()),
-            ],
-            &ffmpeg_flags,
-        )
-        .unwrap_or_else(|_| {
-            clean_abort(tmp_path, options.should_delete_video, video_path);
-        });
-    }
-
-    let frames = read_dir(tmp_path)?
-        .filter_map(Result::ok)
-        .filter(|e| e.file_name() != *"audio.mp3")
-        .map(|entry| entry.path())
-        .collect::<Vec<PathBuf>>();
-
-    println!("\nStarting frame generation ...");
-
-    read_frames(
-        frames,
-        tmp_path,
-        &mut output,
-        options,
-        &should_stop,
-        video_path,
-        fps_frametime,
-    );
-
-    println!(
-        "\n\n\
-        >=== Done! ===<\n\
-        >> Output available at {}",
-        output.display()
-    );
-
-    clean(tmp_path, options.should_delete_video, video_path);
+fn main() -> Res<()> {
+    let program = Arc::new(AsciiCompiler::new(Args::parse())?);
+    register_ctrl_c_handle(program)?;
     Ok(())
 }
 
-fn read_frames(
-    frames: Vec<PathBuf>,
-    tmp_path: &Path,
-    output: &mut PathBuf,
-    options: Options,
-    should_stop: &Arc<AtomicBool>,
-    video_path: &str,
-    (fps, frametime): (usize, usize),
-) {
-    output.set_extension("bapple");
-    let processed = AtomicUsize::new(0);
-    let total = frames.len();
-
-    let mut tar_archive = Builder::new(File::create(output).unwrap());
-
-    let encoded_frames = frames
-        .into_par_iter()
-        .map(|path| {
-            if should_stop.load(Ordering::Relaxed) {
-                pause();
-            }
-            let image = match process_image(&path, options) {
-                Ok(p) => p,
-                Err(error) => {
-                    eprintln!("Image processing failed. This is probably an ffmpeg related issue");
-                    eprintln!("You should try rerunning this program.");
-                    eprintln!("In any case, here's the error message: \n\n{error:?}");
-
-                    clean_abort(tmp_path, options.should_delete_video, video_path); // Prevents littering temporary directory when image processing fails
-                }
-            };
-
-            processed.fetch_add(1, Ordering::Relaxed);
-            let now = processed.load(Ordering::Relaxed);
-
-            print!("\rProcessing: {}% {now}/{total}", (100 * now) / total);
-
-            // Linking
-
-            (path, encode_all(image.as_bytes(), 1).unwrap())
-        })
-        .collect::<Vec<_>>();
-
-    let mut processed = 0;
-
-    // Handle file IO on a single thread to prevent inconsistencies
-    for (path, data) in encoded_frames {
-        processed += 1;
-        print!(
-            "\rLinking: {}% {processed}/{total}",
-            (processed * 100) / total
-        );
-
-        let mut inside_path = PathBuf::from(".");
-        inside_path.set_file_name(path.file_stem().unwrap());
-        inside_path.set_extension("zst");
-
-        add_file(&mut tar_archive, &inside_path, &data).unwrap();
-    }
-
-    // Finally add the audio and metadata to the archive and finish
-    if !options.skip_audio {
-        let mut audio = File::open(tmp_path.join("audio.mp3")).unwrap();
-        let mut data = Vec::new();
-        audio.read_to_end(&mut data).unwrap();
-
-        add_file(&mut tar_archive, "audio.mp3", &data).unwrap();
-    }
-
-    let metadata = ron::Options::default()
-        .to_string_pretty(&Metadata::new(fps, frametime), PrettyConfig::default())
-        .unwrap();
-
-    add_file(&mut tar_archive, "metadata.ron", metadata.as_bytes()).unwrap();
-
-    tar_archive.finish().unwrap();
+fn abort_cleanly(program: Arc<AsciiCompiler>) -> ! {
+    cleanup(program);
+    eprintln!("{YELLOW}Cleanup successful, now aborting...");
+    todo!()
 }
 
-fn process_image(image: &PathBuf, options: Options) -> Result<String, ImageError> {
-    let image = Reader::open(image)?.decode()?;
+fn cleanup(program: Arc<AsciiCompiler>) {
+    eprintln!("\n\n{YELLOW}Cleaning up...");
+    let tmp_dir_path = program.temp_dir.path();
+    remove_dir_all(tmp_dir_path).unwrap_or_else(|_| {
+        panic!(
+            "remove_dir_all() failed. Check for littering on {tmp_dir_path:?}"
+        )
+    });
+}
 
-    let resized_image = image.resize_exact(
-        u32::try_from(options.redimension.0).unwrap(),
-        u32::try_from(options.redimension.1).unwrap(),
-        FilterType::Nearest,
-    );
-
-    let size = resized_image.dimensions();
-
-    let mut res = String::new();
-    let mut last_colorized_pixel = resized_image.get_pixel(size.0 - 1, size.1 - 1);
-    let mut is_first_row_pixel = true;
-
-    for y in 0..size.1 {
-        for x in 0..size.0 {
-            let [r, g, b, _] = resized_image.get_pixel(x, y).0;
-
-            let mut was_colorized = false;
-
-            let brightness = r.max(g).max(b);
-
-            let (thresholds, chars) = if options.boost {
-                (
-                    [5, 10, 15, 20, 25, 40, 200],
-                    [' ', '.', ':', '-', '+', '=', '#'],
-                )
-            } else {
-                (
-                    [20, 40, 80, 100, 130, 200, 250],
-                    [' ', '.', ':', '-', '+', '=', '#'],
-                )
-            };
-
-            let char = chars
-                .iter()
-                .zip(thresholds)
-                .find(|(_, th)| brightness <= *th)
-                .map_or('@', |(&c, _)| c);
-
-            if options.colorize
-                && (max_sub(last_colorized_pixel[0], r) > options.compression_threshold
-                    || max_sub(last_colorized_pixel[1], g) > options.compression_threshold
-                    || max_sub(last_colorized_pixel[2], b) > options.compression_threshold
-                    || is_first_row_pixel)
-                || options.skip_compression
-            {
-                was_colorized = true;
-                write!(
-                    res,
-                    "\x1b[{}8;2;{r};{g};{b}m{}",
-                    match options.style {
-                        BgPaint | BgOnly => 4,
-                        FgPaint => 3,
-                    },
-                    match options.style {
-                        BgPaint | FgPaint => char,
-                        BgOnly => ' ',
-                    }
-                )
-                .unwrap();
-            } else {
-                res.push(match options.style {
-                    BgPaint | FgPaint => char,
-                    BgOnly => ' ',
-                });
-            }
-
-            if was_colorized {
-                last_colorized_pixel.0 = [r, g, b, 255];
-            }
-            is_first_row_pixel = false;
-        }
-        if options.colorize {
-            res.push_str("\x1b[0m\n");
-        } else {
-            res.push('\n');
-        }
-        is_first_row_pixel = true;
-    }
-
-    Ok(res)
+fn register_ctrl_c_handle(program: Arc<AsciiCompiler>) -> Res<()> {
+    ctrlc::set_handler(move || {
+        program.stop_handle.store(true, Relaxed);
+        abort_cleanly(program.clone());
+    })?;
+    Ok(())
 }
--- asciic/src/primitives.rs
+++ asciic/src/primitives.rs
@@ -1,107 +1,32 @@
-use clap::{
-    ErrorKind, ValueEnum,
-    builder::{TypedValueParser, ValueParserFactory},
-    crate_version,
+use std::{path::PathBuf, sync::atomic::AtomicBool};
+
+use tempfile::TempDir;
+
+use crate::{
+    Res,
+    cli::{Args, Style},
 };
-use serde::Serialize;
 
-#[derive(Clone, Copy)]
-#[allow(clippy::struct_excessive_bools)]
-pub struct Options {
-    pub boost: bool,
-    pub compression_threshold: u8,
-    pub redimension: OutputSize,
-    pub skip_compression: bool,
-    pub style: PaintStyle,
-    pub colorize: bool,
-    pub skip_audio: bool,
-    pub should_delete_video: bool,
+pub enum Input {
+    Video(PathBuf),
+    Image(PathBuf),
 }
 
-#[derive(Clone, Copy, Debug, ValueEnum)]
-pub enum PaintStyle {
-    FgPaint,
-    BgPaint,
-    BgOnly,
+pub struct AsciiCompiler {
+    pub stop_handle: AtomicBool,
+    pub temp_dir: TempDir,
+
+    // Set by args from now on ↓
+    input: Input,
+    colorize: bool,
+    no_audio: bool,
+    style: Style,
+    output: PathBuf,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct OutputSize(pub usize, pub usize);
-impl ValueParserFactory for OutputSize {
-    type Parser = OutputSizeParser;
-
-    fn value_parser() -> Self::Parser {
-        OutputSizeParser
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct OutputSizeParser;
-impl TypedValueParser for OutputSizeParser {
-    type Value = OutputSize;
-
-    fn parse_ref(
-        &self,
-        cmd: &clap::Command,
-        _: Option<&clap::Arg>,
-        value: &std::ffi::OsStr,
-    ) -> Result<Self::Value, clap::Error> {
-        let value = value
-            .to_str()
-            .ok_or_else(|| {
-                cmd.clone()
-                    .error(ErrorKind::InvalidUtf8, "Not UTF8, try 216x56.")
-            })?
-            .to_ascii_lowercase();
-
-        let vals = value.split('x').collect::<Vec<_>>();
-        if vals.len() != 2 {
-            return Err(cmd
-                .clone()
-                .error(ErrorKind::InvalidValue, "Wrong pattern, try 216x56."));
-        }
-        let output_size = OutputSize(
-            vals.first()
-                .unwrap()
-                .parse::<usize>()
-                .map_err(|e| cmd.clone().error(ErrorKind::InvalidValue, e.to_string()))?,
-            vals.last()
-                .unwrap()
-                .parse::<usize>()
-                .map_err(|e| cmd.clone().error(ErrorKind::InvalidValue, e.to_string()))?,
-        );
-
-        if output_size.0 > 400 || output_size.1 > 200 {
-            eprintln!("WARN: Usually going too high on frame size makes stuff a bit wonky.");
-        }
-
-        // I mean, why would you want to resize the image to u32::MAX?
-        assert!(output_size.0 < u32::MAX as usize || output_size.1 < u32::MAX as usize);
-
-        Ok(output_size)
-    }
-}
-
-#[non_exhaustive]
-#[derive(Serialize)]
-pub struct Metadata {
-    frametime: usize, // Pass frametime directly, so we don't have issues dealing with fps values like: 29.33
-    fps: usize,       // Old value, will still be passed for old bplay versions.
-    asciic_version: &'static str,
-    comment: &'static str,
-}
-
-const COMMENT: &str = "If you're reading this, you're probably using an outdated version of bplay.\n\
-                       Go ahead and update it on https://github.com/S0raWasTaken/bapple_player if you want.\n\
-                       It sets the FPS automatically now, so go on, have fun.";
-
-impl Metadata {
-    pub fn new(fps: usize, frametime: usize) -> Self {
-        Self {
-            frametime,
-            fps,
-            asciic_version: crate_version!(),
-            comment: COMMENT,
-        }
+impl AsciiCompiler {
+    pub fn new(args: Args) -> Res<Self> {
+        let input = args.handle_input();
+        todo!();
     }
 }
