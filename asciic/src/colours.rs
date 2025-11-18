d877b169b31907d4305cd518c9b461a1c4e94a0b
S0ra <s0ra@duck.com>
2025-11-17 19:04:11 -0500

    chore: Remove `#[allow(dead_code)]` from `colours.rs`

--- asciic/src/colours.rs
+++ asciic/src/colours.rs
@@ -1,13 +1,12 @@
-#![allow(dead_code)]
 // Colours for console messages
 
 pub const BOLD: &str = "\x1b[1m";
 pub const RESET: &str = "\x1b[0m";
-pub const BLACK: &str = "\x1b[30m";
+// pub const BLACK: &str = "\x1b[30m";
 pub const RED: &str = "\x1b[31m";
-pub const GREEN: &str = "\x1b[32m";
+// pub const GREEN: &str = "\x1b[32m";
 pub const YELLOW: &str = "\x1b[33m";
-pub const BLUE: &str = "\x1b[34m";
-pub const MAGENTA: &str = "\x1b[35m";
-pub const CYAN: &str = "\x1b[36m";
-pub const WHITE: &str = "\x1b[37m";
+// pub const BLUE: &str = "\x1b[34m";
+// pub const MAGENTA: &str = "\x1b[35m";
+// pub const CYAN: &str = "\x1b[36m";
+// pub const WHITE: &str = "\x1b[37m";
